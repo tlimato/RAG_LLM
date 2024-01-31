80b5b7bae946f70f5631847dbccd771bb1551301
tyson <tlimato@uwyo.edu>
2024-01-30 19:30:23 -0700

    Added formatting and essential setup files for reproducibility.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='RAG_LLM',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='tyson',
+    author_email='',
+    description=''
+)
